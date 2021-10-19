06f4ae0f1d662712f9b2f5ab4fb1e3c42aeb7895
Dayana <dayanadiasgarcia@gmail.com>
2021-10-19 13:41:28 -0300

    Requisitos 3,4 e 5

--- src/challenges.js
+++ src/challenges.js
@@ -16,14 +16,14 @@ function calcArea(base, height) {
 // Desafio 3
 function splitSentence(arrStr) {
   let frase = arrStr.split(' ');
-  return console.log(frase);
+  return frase;
 }
 
 // Desafio 4
 function concatName(arrString) {
   let last = arrString[arrString.length - 1];
   let first = arrString[0];
-  return (console.log(last, first));
+  return (last, first);
 }
 
 // Desafio 5
@@ -35,28 +35,9 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 
-function highestCount(arrNumbers) {
-
-  let highNumber = arrNumbers[0];
-  let qntsMaior = 0;
-
-  for (let i of arrNumbers) {
-    if (i > highNumber) {
-      highNumber = i;
-    }
-
-  }
-  console.log(highNumber);
-
-  for (let cont = 0; cont < arrNumbers.length; cont++) {
-    if (arrNumbers[cont] == cont) {
-      qntsMaior++;
-    }
-  }
-  return console.log(qntsMaior);
-
+function highestCount() {
+  ///
 }
-highestCount([1, 2, 3, 5, 5, 4, 6, 7, 7, 7, 7]);
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
