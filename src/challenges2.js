0c6dd3a3d14c8982eba14b0106ea1ef1b51b80d5
Dayana <dayanadiasgarcia@gmail.com>
2022-06-15 18:10:09 -0300

    Refatorando 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -57,20 +57,31 @@ function generatePhoneNumber(numbers) {
   return mask;
 }
 
-// // Desafio 12
-// function triangleCheck() {
-//   // seu código aqui
-// }
+// Desafio 12
+function triangleCheck(l1, l2, l3) {
+  const sub = [l1 - l2, l1 - l3, l2 - l3];
+  const sum = [l1 + l2, l1 + l3, l2 + l3];
+  if (l1 < sum[2] && l1 > Math.abs(sub[2])
+    || l2 < sum[1] && l2 > Math.abs(sub[1])
+    || l3 < sum[0] && l3 > Math.abs(sub[0])) {
+    return true;
+  }
+  return false;
+}
 
-// // Desafio 13
-// function hydrate(comanda) {
-//   comanda.replace(/\D/g, '');
-//   console.log(comanda);
-// }
+// Desafio 13
+function hydrate(comanda) {
+  let regex = /\d+/g;
+  let matches = comanda.match(regex); // creates array from matches
+  let arrNum = matches.toString().split(',').map((item) => parseInt(item, 10));
+  let water = arrNum.reduce((prevNum, currNum) => prevNum + currNum, 0);
+  if (water > 1) return `${water} copos de água`;
+  return `${water} copo de água`;
+}
 
 module.exports = {
   generatePhoneNumber,
   techList,
-  // hydrate,
-  // triangleCheck,
+  hydrate,
+  triangleCheck,
 };
